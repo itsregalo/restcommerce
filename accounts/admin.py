60d8952adc202529f9837135196cb6fbc28b4c5e
retech <itsregalo047@gmail.com>
2021-08-20 21:56:55 +0300

    account signals firing 100%

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,12 +1,29 @@
 from django.contrib import admin
-from .models import *
+from accounts.models import (CustomUser, AdminUser, StaffUser,
+                         MerchantUser, CustomerUser, PhoneNumber, Profile
+                         )
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
 
 
-admin.site.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {
+            "fields": (
+                'is_superuser',
+                'is_staff',
+                'is_merchant',
+                'is_customer',
+            ),
+        }),
+    )
+    
+    
+admin.site.register(CustomUser, CustomUserAdmin)
+
 admin.site.register(AdminUser)
 admin.site.register(StaffUser)
 admin.site.register(MerchantUser)
 admin.site.register(CustomerUser)
-admin.site.register(Profile)
 admin.site.register(PhoneNumber)
+admin.site.register(Profile)
