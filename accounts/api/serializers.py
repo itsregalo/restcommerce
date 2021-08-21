2de40c0cf8f47da6281049b2ae593628c7209f5d
retech <itsregalo047@gmail.com>
2021-08-21 13:02:41 +0300

    registration working perfectly

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -1,28 +1,30 @@
 from rest_framework import serializers
 from accounts.models import CustomUser
 
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 class RegistrationSerializer(serializers.ModelSerializer):
 
-    password2 = serializers.CharField(style={'input-type':'password'}, write_only=True)
+    password2 = serializers.CharField(style={'input_type':'password'}, write_only=True)
     class Meta:
-        model = CustomUser
+        model = User
         fields = ['email', 'username','password','password2']
 
         extra_kwargs = {
             'password':{'write_only':True}
         }
 
-        def save(self):
-            user = CustomUser(
-                email = self.validated_data['email'],
-                username = self.validated_data['username'],
-            )
-            password = self.validated_data['password']
-            password2 = self.validate_data['password2']
+    def save(self):
+        user = User.objects.create(
+            email = self.validated_data['email'],
+            username = self.validated_data['username'],
+        )
+        password = self.validated_data['password']
+        password2 = self.validated_data['password2']
 
-            if password != password2:
-                raise serializers.ValidationError({'password':'password must match'})
-            user.set_password(password)
-            user.save()
-            return user
+        if password != password2:
+            raise serializers.ValidationError({'password':'password must match'})
+        user.set_password(password)
+        user.save()
+        return user
