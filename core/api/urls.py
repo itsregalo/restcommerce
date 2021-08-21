b65b0914939b6f65c5fcbe71c4af827120af3924
retech <itsregalo047@gmail.com>
2021-08-21 10:17:36 +0300

    changed views from CBV to FBV

--- core/api/urls.py
+++ core/api/urls.py
@@ -1,17 +1,16 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
-from .views import ProductList, ProductDetail, CategoryList
+from .views import *
 
 app_name = 'api-core'
 
 urlpatterns = [
-    path('products/', ProductList.as_view(), name='product-list'),
-    path('product/<int:pk>/', ProductDetail.as_view(), name='product-detail'),
-    path('product/<int:pk>/update/', ProductDetail.as_view(), name='product-update'),
-    path('product/<int:pk>/delete/', ProductDetail.as_view(), name='product-delete'),
-    path('product/create/', ProductDetail.as_view(), name='product-create'),
+    path('products/', ProductList, name='product-list'),
+    path('product/<int:pk>/', ProductDetail, name='product-detail'),
+    path('product/<int:pk>/update/', ProductUpdate, name='product-update'),
+    path('product/create/', ProductCreate, name='product-create'),
 
-    path('category/', CategoryList.as_view(), name='category-list'),
+    path('category/', CategoryList, name='category-list'),
     ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- core/api/views.py
+++ core/api/views.py
@@ -1,7 +1,6 @@
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 from core.models import Category, Product, ProductMedia
 from .serializers import (
@@ -10,66 +9,60 @@ from .serializers import (
                     ProductMediaSerializer
                     )
 
+@api_view(['GET'])
+def ProductList(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
 
-class ProductList(APIView):
-    """
-    List all products, or create a new product.
-    """
-    def get(self, request, format=None):
-        products = Product.objects.all()
-        serializer = ProductSerializer(products, many=True)
-        return Response(serializer.data)
+@api_view(['GET', 'DELETE'])
+def ProductDetail(request, slug, pk):
+    try:
+        product = Product.objects.get(pk=pk)
+    except Product.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
-    def post(self, request, format=None):
-        serializer = ProductSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class ProductDetail(APIView):
-    """
-    Retrieve, update or delete a product instance.
-    """
-    def get_object(self, pk):
-        try:
-            return Product.objects.get(pk=pk)
-        except Product.DoesNotExist:
-            raise Http404
-
-    def get(self, request, pk, format=None):
-        product = self.get_object(pk)
+    if request.method == 'GET':
         serializer = ProductSerializer(product)
         return Response(serializer.data)
+    elif request.method == 'DELETE':
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
-    def put(self, request, pk, format=None):
-        product = self.get_object(pk)
+@api_view(['POST'])
+def ProductCreate(request):
+    serializer = ProductSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['PUT'])
+def ProductUpdate(request, slug,pk):
+    try:
+        product = Product.objects.get(pk=pk)
+    except Product.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'PUT':
         serializer = ProductSerializer(product, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, pk, format=None):
-        product = self.get_object(pk)
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-
-class CategoryList(APIView):
-    """
-    List all categories, or create a new category.
-    """
-    def get(self, request, format=None):
+@api_view(['GET','POST'])
+def CategoryList(request):
+    if request.method == 'GET':
         categories = Category.objects.all()
         serializer = CategorySerializer(categories, many=True)
         return Response(serializer.data)
-
-    def post(self, request, format=None):
+    elif request.method == 'POST':
         serializer = CategorySerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
                         
