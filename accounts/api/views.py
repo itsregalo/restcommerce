8fc126bd153fa634ceb22934c4c66ef7aa22d112
retech <itsregalo047@gmail.com>
2021-08-21 12:23:33 +0300

    user account registration

--- accounts/api/views.py
+++ accounts/api/views.py
@@ -5,3 +5,15 @@ from rest_framework.decorators import api_view
 from .serializers import RegistrationSerializer
 
 @api_view(['POST'])
+def RegistrationView(request):
+    if request.method == 'POST':
+        serializer = RegistrationSerializer(data=request.data)
+        data = {}
+
+        if serializer.is_valid():
+            user = serializer.save()
+            data['response'] = "User registered successfully"
+            data['email'] = user.email
+            data['username'] = user.username
+            return Response(data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
