129b01a497f364d82c3558b51336db2f736d9b8f
retech <itsregalo047@gmail.com>
2021-08-21 12:17:26 +0300

    user account serializer

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -1,1 +1,28 @@
-from django.contrib.auth.models import User
+from rest_framework import serializers
+from accounts.models import CustomUser
+
+
+class RegistrationSerializer(serializers.ModelSerializer):
+
+    password2 = serializers.CharField(style={'input-type':'password'}, write_only=True)
+    class Meta:
+        model = CustomUser
+        fields = ['email', 'username','password','password2']
+
+        extra_kwargs = {
+            'password':{'write_only':True}
+        }
+
+        def save(self):
+            user = CustomUser(
+                email = self.validated_data['email'],
+                username = self.validated_data['username'],
+            )
+            password = self.validated_data['password']
+            password2 = self.validate_data['password2']
+
+            if password != password2:
+                raise serializers.ValidationError({'password':'password must match'})
+            user.set_password(password)
+            user.save()
+            return user
